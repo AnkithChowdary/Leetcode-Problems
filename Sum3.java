3a2e1f425a00d3c0f8496cc728862714da086453
Ankith <ankithc29@gmail.com>
2024-06-19 21:01:48 +0530

    Update

--- Sum3.java
+++ Sum3.java
@@ -6,8 +6,14 @@ class Solution {
         Set<List<Integer>> s = new HashSet<>();
         int n = nums.length;
 
-        for (int i = 0; i < n - 3; i++) {
-            for (int j = i + 1; j < n - 2; j++) {
+        for (int i = 0; i < n; i++) {
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            for (int j = i + 1; j < n; j++) {
+                if(j>i+1&&nums[j]==nums[j-1]){
+                    continue;
+                }
                 int k = j + 1;
                 int l = n - 1;
 
