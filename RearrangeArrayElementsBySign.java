c3f60c9cfa118d6709c56cc156d85a47b25a70e2
Ankith <ankithc29@gmail.com>
2024-06-20 20:24:47 +0530

    Update

---
+++ RearrangeArrayElementsBySign.java
@@ -0,0 +1,19 @@
+
+class Solution {
+  public int[] rearrangeArray(int[] nums) {
+      int n=nums.length;
+      int []arr=new int[n];
+      int j=0,k=1;
+      for(int i=0;i<n;i++){
+          if(nums[i]>=0){
+              arr[j]=nums[i];
+              j=j+2;
+          }else{
+
+              arr[k]=nums[i];
+              k=k+2;
+          }
+      }
+      return arr;
+  }
+}
