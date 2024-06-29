fbf7ebf74aedf35078e637e111b68742b3f03562
AnkithChowdary <ankithc29@gmail.com>
2024-06-29 23:08:43 +0530

    Update

---
+++ ValidPangram.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean checkIfPangram(String sentence) {
+      int[] ans = new int[26];
+      for(int i = 0;i<sentence.length();i++)
+          ans[sentence.charAt(i)-'a']++;
+      
+      for(int i : ans)
+          if(i < 1) 
+              return false;
+      
+      return true;
+  }
+}
