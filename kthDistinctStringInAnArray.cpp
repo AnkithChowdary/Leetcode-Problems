a94b009809a3b7e95d77890a962e5c8037fb1a4c
AnkithChowdary <ankithc29@gmail.com>
2024-08-05 14:43:59 +0530

    Update

---
+++ kthDistinctStringInAnArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        unordered_map<string,int> mp;
+      
+        for (string str:arr){
+            mp[str]++;
+        }
+      
+        int count=0;
+        for (string str:arr){
+            if (mp[str]==1){
+                count++;
+                if (count==k) {
+                    return str;
+                }
+            }
+        }
+        
+        return "";
+    }
+};
