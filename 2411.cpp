2985300ad68bef7aed1ffdea1a4cb03a8a695f69
AnkithChowdary <ankithc29@gmail.com>
2025-07-29 23:54:53 +0530

    Implement smallest subarrays calculation

---
+++ 2411.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n=nums.size();
+
+        vector<int>res(n);
+
+        vector<int>setBitIdx(32,-1);
+        
+      
+        for(int i=n-1;i>=0;i--){
+              int endIdx=i;
+            for(int j=0;j<32;j++){
+                if(!(nums[i]&(1<<j))){
+                    if(setBitIdx[j]!=-1){
+                        endIdx=max(endIdx,setBitIdx[j]);
+                    }
+                }else{
+                    setBitIdx[j]=i;
+                }
+            }
+            res[i]=endIdx-i+1;
+            
+        }
+        return res;
+
+    }
+};
